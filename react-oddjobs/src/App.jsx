ea4929c18508cb7147e85acf564986bd32a405c3
GrdnhffMann <138700965+LJynver@users.noreply.github.com>
2023-11-26 12:43:32 +0800

    Odd-Jobs HTML version converted to React

--- react-oddjobs/src/App.jsx
+++ react-oddjobs/src/App.jsx
@@ -4,24 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <header className='header-oddjobs'>
-        <a href="" className='thumbtitle'>
-          <div>
-            <span className='oddtitle'>ODD</span>
-            <span className='jobstitle'>JOBS</span>
-          </div>
-        </a>
-
-        <nav>
-          <ul>
+    <div class="header">
+      <div class="inner_header"> 
+          <a class="logo_container" href="index.html">
+              <div class="logo_box">
+                  <h1>ODD<span>JOBS</span></h1>
+              </div>
+          </a>
+          <ul class="nav">
+              <a href="login.html" onclick="changeColor(this)"><li>SIGN IN</li></a>
           </ul>
-        </nav>
+      </div>
+  </div>
 
-      </header>
+  <div class="container">
+        
+        <div class="card-form">
+            <form>
+                <h1>CHANGE YOUR PASSWORD</h1>
+                <div class="content">
+
+                    <div class="input-field">
+                      <input type="text" placeholder="New Password" autocomplete="nope"/>
+                    </div>
+                    <div class="input-field">
+                      <input type="text" placeholder="Confirm Password" autocomplete="nope"/>
+                    </div>
+                    <button class="submit-button">SUBMIT</button>
+                </div>
+            </form>
+        </div>
+    </div>
     </>
   )
 }
